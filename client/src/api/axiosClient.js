fec419cfa6a2be1c53ac0c72402ae6cab1cedf1d
Sonhong2410 <sonlatentui2410@gmail.com>
2025-12-01 21:47:45 +0700

    Son update orderpage

---
+++ client/src/api/axiosClient.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+// Tạo axios instance với cấu hình chung cho cả nhóm
+const axiosClient = axios.create({
+  baseURL: 'http://localhost/Shoppe/server/api', // Đường dẫn đến API PHP
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptor cho request
+axiosClient.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor cho response
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.error('API Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
